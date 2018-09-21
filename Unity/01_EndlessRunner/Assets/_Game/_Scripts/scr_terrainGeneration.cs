3e1995a42908b316b9ad9b37f7c5847c78064748
NMMN1909 <danda_fluem@hotmail.com>
2018-09-21 16:19:36 +0200

    Update 0.5
    Infinite Terrain Generator

---
+++ Unity/01_EndlessRunner/Assets/_Game/_Scripts/scr_terrainGeneration.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_terrainGeneration : MonoBehaviour {
+
+    //reference
+    public GameObject terrainPrefab;
+    public Transform spawnTerrainLoc;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        terrainPrefab = Instantiate(terrainPrefab, spawnTerrainLoc.position, Quaternion.identity);
+    }
+
+}
---
+++ Unity/01_EndlessRunner/Assets/_Game/_Scripts/scr_terrainMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_terrainMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float terrainMovSpeed;
+
+    private void Start()
+    {
+        terrainMovSpeed = -5f;
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * terrainMovSpeed * Time.deltaTime);
+    }
+
+}
