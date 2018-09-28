0a89841c133cd075736c885b59ba55701f8558c4
tobias02121999 <hedgefoggames@outlook.com>
2018-09-28 12:25:01 +0200

    Update 1.2

---
+++ Unity/01_EndlessRunner/Assets/scr_gameManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_gameManager : MonoBehaviour {
+
+    // Initialize the public variables
+    public float globalMovementSpeed;
+}
